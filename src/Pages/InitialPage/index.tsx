04c7825d4bae096af96ad5e6266bd190e3e74b80
Joanne Stephany <joannestephany@hotmail.com>
2025-02-10 17:06:43 -0300

    wip: componentizaçao
    - contexto criado para os graficos na pagina inicial
    - compenente referente ao card com grafico

--- src/Pages/InitialPage/index.tsx
+++ src/Pages/InitialPage/index.tsx
@@ -15,7 +15,7 @@ import { AplicationContext } from "../../Context/Aplication/context";
 import http from "../../Services/axios";
 import { getYear } from "../../Services/localstorage";
 
-import { Column, Padding, Row } from "../../Styles/styles";
+import { Padding, Row } from "../../Styles/styles";
 import { PropsAplicationContext } from "../../Types/types";
 
 import { InitialPageModel } from "./initialPageModel";
@@ -24,6 +24,9 @@ import { getInitialPageModel } from "../../Controller/controllerInicialPage";
 import { ChartMatriculated } from "./Components/ChartMatrticulated/chartMatriculated";
 import { ChartStatus } from "./Components/ChartStatus/chartStatus";
 import { ROLE } from "../../Controller/controllerGlobal";
+import ChartsProvider, { ChartsContext } from "../../Context/Charts/context";
+import type { ChartsProps } from "../../Context/Charts/context";
+import { ChartCard } from "./Components/ChartCard";
 
 const subtractMonths = (date: Date, months: number): Date => {
   const newDate = new Date(date);
@@ -40,10 +43,11 @@ const InitialPage = () => {
   const [ts, setTs] = useState<number[] | undefined>();
   const [initialPageData, setInitialPageData] = useState<InitialPageModel>();
 
+  const chartData = useContext(ChartsContext) as ChartsProps;
+
   useEffect(() => {
     if (propsAplication.project === undefined) return;
     setTs(propsAplication.project?.map((item) => item.id));
-    console.log("teste2");
     setDates([subtractMonths(new Date(Date.now()), 6), new Date(Date.now())]);
 
     const fetchData = async () => {
@@ -79,157 +83,147 @@ const InitialPage = () => {
   };
 
   return (
-    <ContentPage
-      title={`Bem vindo, ${propsAplication.user?.name}!`}
-      description="Visualização dos dados gerais do meuBen."
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "stretch", // Garante que todos os itens tenham a mesma altura
-          width: "100%",
-          gap: "10px", // Define o espaço entre os elementos
-          flexWrap: "wrap", // Permite que os itens se ajustem em diferentes linhas se necessário
-        }}
+    <ChartsProvider>
+      <ContentPage
+        title={`Bem vindo, ${propsAplication.user?.name}!`}
+        description="Visualização dos dados gerais do meuBen."
       >
         <div
           style={{
             display: "flex",
+            justifyContent: "space-between",
             alignItems: "stretch",
+            width: "100%",
             gap: "10px",
-            flex: 1,
+            flexWrap: "wrap",
           }}
         >
-          {propsAplication.project && (
-            <div style={{ flex: 2, display: "flex", flexDirection: "column" }}>
-              <MultiSelectComponet
-                options={[...propsAplication.project]}
-                optionsLabel="name"
-                optionsValue="id"
-                value={ts}
-                onChange={(e) => setTs(e.target.value)}
-                placerholder="Filtrar por Tecnologia Social"
-              />
-            </div>
-          )}
-          {propsAplication.project && (
-            <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-              <CalendarComponent
-                value={dates}
-                onChange={(e: any) => setDates(e.value)}
-                selectionMode="range"
-                placeholder="Selecione o período"
-                dateFormat="dd/mm/yy"
-                name="dates"
-              />
-            </div>
-          )}
-        </div>
-
-        {propsAplication.user?.role === ROLE.ADMIN && (
-          <Row
-            id="end"
+          <div
             style={{
-              marginLeft: "auto",
-              flexShrink: 0,
               display: "flex",
-              alignItems: "center",
+              alignItems: "stretch",
               gap: "10px",
+              flex: 1,
             }}
           >
-            <Button
-              label="Baixar CSV"
-              icon="pi pi-download"
-              iconPos="left"
-              onClick={downloadCSV}
+            {propsAplication.project && (
+              <div
+                style={{ flex: 2, display: "flex", flexDirection: "column" }}
+              >
+                <MultiSelectComponet
+                  options={[...propsAplication.project]}
+                  optionsLabel="name"
+                  optionsValue="id"
+                  value={ts}
+                  onChange={(e) => setTs(e.target.value)}
+                  placerholder="Filtrar por Tecnologia Social"
+                />
+              </div>
+            )}
+            {propsAplication.project && (
+              <div
+                style={{ flex: 1, display: "flex", flexDirection: "column" }}
+              >
+                <CalendarComponent
+                  value={dates}
+                  onChange={(e: any) => setDates(e.value)}
+                  selectionMode="range"
+                  placeholder="Selecione o período"
+                  dateFormat="dd/mm/yy"
+                  name="dates"
+                />
+              </div>
+            )}
+          </div>
+
+          {propsAplication.user?.role === ROLE.ADMIN && (
+            <Row
+              id="end"
+              style={{
+                marginLeft: "auto",
+                flexShrink: 0,
+                display: "flex",
+                alignItems: "center",
+                gap: "10px",
+              }}
+            >
+              <Button
+                label="Baixar CSV"
+                icon="pi pi-download"
+                iconPos="left"
+                onClick={downloadCSV}
+              />
+            </Row>
+          )}
+        </div>
+
+        <Padding padding="16px" />
+        <div className="grid">
+          <div className="col-12 md:col-4 lg:col-2">
+            <CardQuant
+              title="Total de Ts"
+              quant={initialPageData?.cardsData?.totalUserSocialTechnologies!}
+              color="navy_blue"
             />
-          </Row>
-        )}
-      </div>
-
-      <Padding padding="16px" />
-      <div className="grid">
-        <div className="col-12 md:col-4 lg:col-2">
-          <CardQuant
-            title="Total de Ts"
-            quant={initialPageData?.cardsData?.totalUserSocialTechnologies!}
-            color="navy_blue"
-          />
-        </div>
-        <div className="col-12 md:col-4 lg:col-2">
-          <CardQuant
-            title="Total de Projetos"
-            quant={initialPageData?.cardsData?.totalProjects!}
-            color="blue"
-          />
-        </div>
-        <div className="col-12 md:col-4 lg:col-2">
-          <CardQuant
-            title="Total de Turmas"
-            quant={initialPageData?.cardsData?.totalClassrooms!}
-            color="orange"
-          />
-        </div>
-        <div className="col-12 md:col-4 lg:col-2">
-          <CardQuant
-            title="Total de matrículas"
-            quant={initialPageData?.cardsData?.totalRegisterClassrooms!}
-            color="navy_blue"
-          />
-        </div>
-        <div className="col-12 md:col-4 lg:col-2">
-          <CardQuant
-            title="Total de matrículas confirmadas"
-            quant={initialPageData?.cardsData?.approvedRegisterClassrooms!}
-            color="blue"
-          />
-        </div>
-        <div className="col-12 md:col-4 lg:col-2">
-          <CardQuant
-            title="Total de encontros"
-            quant={initialPageData?.cardsData?.totalMeetings!}
-            color="orange"
-          />
-        </div>
-      </div>
-
-      <Row>
-        <div
-          className="card col-12 md:col-6 lg:col-6"
-          style={{ padding: "20px" }}
-        >
-          <Row id="start">
-            <Column>
-              <h2>Gráfico de Matrículas Total</h2>
-              <Padding padding="8px" />
-            </Column>
-          </Row>
-
-          <div>
-            <ChartMatriculated dates={dates} ts={ts} />
+          </div>
+          <div className="col-12 md:col-4 lg:col-2">
+            <CardQuant
+              title="Total de Projetos"
+              quant={initialPageData?.cardsData?.totalProjects!}
+              color="blue"
+            />
+          </div>
+          <div className="col-12 md:col-4 lg:col-2">
+            <CardQuant
+              title="Total de Turmas"
+              quant={initialPageData?.cardsData?.totalClassrooms!}
+              color="orange"
+            />
+          </div>
+          <div className="col-12 md:col-4 lg:col-2">
+            <CardQuant
+              title="Total de matrículas"
+              quant={initialPageData?.cardsData?.totalRegisterClassrooms!}
+              color="navy_blue"
+            />
+          </div>
+          <div className="col-12 md:col-4 lg:col-2">
+            <CardQuant
+              title="Total de matrículas confirmadas"
+              quant={initialPageData?.cardsData?.approvedRegisterClassrooms!}
+              color="blue"
+            />
+          </div>
+          <div className="col-12 md:col-4 lg:col-2">
+            <CardQuant
+              title="Total de encontros"
+              quant={initialPageData?.cardsData?.totalMeetings!}
+              color="orange"
+            />
           </div>
         </div>
-        {/* Adicionar espaço separador aqui*/}
-        <Padding padding="10px" />
-        <div
-          className="card col-12 md:col-6 lg:col-6"
-          style={{ padding: "20px" }}
-        >
-          <Row id="start">
-            <Column>
-              <h2>Gráfico de Turmas</h2>
-              <Padding padding="8px" />
-            </Column>
-          </Row>
-          <div>
-            <ChartStatus dates={dates} ts={ts} />
-          </div>
-        </div>
-      </Row>
 
-      <Padding padding="16px" />
-    </ContentPage>
+        <div
+          className="grid"
+          style={{ display: "flex", justifyContent: "space-between" }}
+        >
+          <ChartCard
+            title="Gráfico de Matrículas Total"
+            type="line"
+            dates={dates}
+            ts={ts}
+          />
+          <ChartCard
+            title="Status de Matrículas"
+            type="bar"
+            dates={dates}
+            ts={ts}
+          />
+        </div>
+
+        <Padding padding="16px" />
+      </ContentPage>
+    </ChartsProvider>
   );
 };
 
